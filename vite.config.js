0820549aaeab2e04d920e6d653be7beddad37eb8
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-10 10:10:40 +1100

    test: changed base path of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/staff-board/', // Add this line to set the base path
+  // base: '/staff-board/', // Add this line to set the base path
+  base: './',
   build: {
     outDir: 'dist', // This ensures the output directory remains consistent
   },
