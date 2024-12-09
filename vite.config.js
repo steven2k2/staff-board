6c480100ab21f0aac21cb1a618d01b1af3f37088
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-10 10:30:37 +1100

    refactored: paths

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   base: '/staff-board/',
-   build: {
-    outDir: 'dist', // This ensures the output directory remains consistent
-  },
+   base: '/staff-board/'
 });
